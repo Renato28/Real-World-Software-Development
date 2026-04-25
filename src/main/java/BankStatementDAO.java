7741a7ffab4972b4574681771fe291be0e7e2af7
Renato Nobrega <renatonobrega87@gmail.com>
2026-04-25 19:10:36 -0300

    implementando a classe BankStatementDAO

---
+++ src/main/java/BankStatementDAO.java
@@ -0,0 +1,26 @@
+import br.com.github.renato28.chapter_02.BankTransaction;
+
+import java.time.LocalDate;
+
+public class BankStatementDAO {
+
+    public BankTransaction create(final LocalDate date, final double amount, final String description) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction read(final long id) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction update(final long id) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    public BankTransaction delete(final BankTransaction transaction) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+}
