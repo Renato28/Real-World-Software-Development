3d1d61874267b4fbe1effb86fb7f95c21986761a
Renato Nobrega <renatonobrega87@gmail.com>
2026-04-25 18:48:17 -0300

    implementando a classe BankStatementAnalyzer

---
+++ src/main/java/br/com/github/renato28/chapter_02/BankStatementAnalyzer.java
@@ -0,0 +1,35 @@
+package br.com.github.renato28.chapter_02;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Month;
+import java.util.List;
+
+public class BankStatementAnalyzer {
+    private static final String RESOURCES = "src/main/resources/";
+
+    public void analyze(final String filename, final BankStatementParser bankStatementParser) throws IOException {
+
+        final Path path = Paths.get(RESOURCES + filename);
+        final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFrom(lines);
+
+        final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
+
+    }
+
+    private static void collectSummary(final BankStatementProcessor bankStatementProcessor) {
+
+        System.out.println("The total for all transactions is "
+                + bankStatementProcessor.calculateTotalAmount());
+        System.out.println("The total for transactions in January is "
+                + bankStatementProcessor.calculatTotalInMonth(Month.JANUARY));
+        System.out.println("The total for transactions in February is "
+                + bankStatementProcessor.calculatTotalInMonth(Month.FEBRUARY));
+        System.out.println("The total salary received is "
+                + bankStatementProcessor.calculateTotalForCategory("Salary"));
+    }
+}
