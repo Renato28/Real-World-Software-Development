f6f234b5be0aba8e312d965319d63cc430555340
Renato Nobrega <renatonobrega87@gmail.com>
2026-04-25 19:12:52 -0300

    implementando a classe BankStatementFullParser

---
+++ src/main/java/br/com/github/renato28/chapter_02/BankStatementFullParser.java
@@ -0,0 +1,20 @@
+package br.com.github.renato28.chapter_02;
+
+public class BankStatementFullParser {
+
+    BankTransaction parseFromCSV(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    BankTransaction parserFromJSON(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    BankTransaction parseFromXML(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+}
